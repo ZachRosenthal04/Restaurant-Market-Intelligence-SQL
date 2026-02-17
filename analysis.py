582bdf70b7030f0ccac3803ef08d2f8c57ea916f
Zach Rosenthal <140099747+ZachRosenthal04@users.noreply.github.com>
2026-02-17 15:59:55 -0500

    Update analysis.py
    Normalized the state abreviations to make joining easier
    Added the population dataset to be able to query off it and a state_by_state efficieny score as seen through the dollars per person in each state.

--- analysis.py
+++ analysis.py
@@ -15,16 +15,48 @@ conn = sqlite3.connect('restaurant_stats.db')
 
 def load_data():
     """Loads raw CSV data into a centralized SQL database."""
-    # Note: Replace file paths with local paths if not using Google Colab
     top250_path = 'Top250.csv' 
     ind100_path = 'Independence100.csv'
+      census_file_path = 'us_pop_by_state_2020.csv'    
+         
     
     df_top250 = pd.read_csv(top250_path)
     df_ind100 = pd.read_csv(ind100_path)
+    df_pop = pd.read_csv(census_file_path)
     
     df_top250.to_sql('top250', conn, index=False, if_exists='replace')
-    df_ind100.to_sql('ind100', conn, index=False, if_exists='replace')
-    print("Database Initialized: 'top250' and 'ind100' tables created.")
+
+    # Strip periods from the State column (e.g., 'N.Y.' becomes 'NY')
+    df_ind['State'] = df_ind['State'].str.replace('.', '', regex=False).str.strip()
+
+    # The 'Translation' dictionary for your specific dataset quirks
+    state_fix_map = {
+         'Fla': 'FL',
+         'Ill': 'IL',
+         'Nev': 'NV',
+         'Ind': 'IN',
+         'Pa': 'PA',
+         'Calif': 'CA',
+         'Ga': 'GA',
+         'Mich': 'MI',
+         'Mass': 'MA',
+         'Ore': 'OR',
+         'Tenn': 'TN',
+         'Colo': 'CO',
+         'Va': 'VA',
+         'Texas': 'TX'
+         }
+
+         # Apply the map to the 'State' column
+         # .fillna keeps original values if they aren't in the map (like 'NY' or 'NJ')
+         df_ind['State'] = df_ind['State'].map(state_fix_map).fillna(df_ind['State'])
+
+         # This adds a second table called 'ind100' to your existing database
+         df_ind.to_sql('ind100', conn, index=False, if_exists='replace')
+         df_ind100.to_sql('ind100', conn, index=False, if_exists='replace')
+
+         df_pop.to_sql('state_population', conn, index=False, if_exists='replace')
+         print("Database Initialized: 'top250','ind100', 'state_population tables created.")
 
 # --- 2. THE 'ADAPTIVE GROWTH' MODEL ---
 def run_market_analysis():
